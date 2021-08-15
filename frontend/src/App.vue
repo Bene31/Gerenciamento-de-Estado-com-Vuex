541cacd401ee943f751e3154664c5997c603f2d2
Arthur Terra <terra.saha@hotmail.com>
2021-08-15 12:29:16 -0300

    vuex

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1> To-do </h1>
+  <ul>
+    <li v-for="todo in $allTodos" :key="todo.id">
+      {{todo.text}}
+    </li>
+  </ul>
+
+  <h1> Done </h1>
+  <ul>
+    <li v-for="todo in $doneTodos" :key="todo.id">
+      {{todo.text}}
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  created(){
+    this.$store.dispatch('fetchTodos')
+  },
+  computed:{
+    $allTodos(){
+      return this.$store.getters.$allTodos
+    },
+    $doneTodos(){
+      return this.$store.getters.$doneTodos
+    }
+  }
+};
+</script>
+
+
